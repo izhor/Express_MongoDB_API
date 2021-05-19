a17d354c9223cbf1538ce08256dd5c4da10cc22d
izhor <alif.zhorif@gmail.com>
2021-05-19 12:59:34 +0700

    Mongodb_Express

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,6 +3,30 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
+
+//connecting to mongodb
+
+var your_MongoDB_Atlas_url = ''; /**
+ * 
+for using connection to your mongoDB atlas, here some steps i can provide:
+1. sign in/up into https://cloud.mongodb.com/
+2. go to your cluster, if you don't have any create one(you only able to make one free cluster with 512MB storage)
+3. press 'CONNECT' button below your cluster name
+4. select 'Connect your application', then select 'Node.js' for driver, and '3.6 or later' for version
+5. then you'll be given the link to your database, make sure to replace <password> tags with your mongoDB cluster password
+
+ */
+var local_MongoDB_url = 'mongodb://127.0.0.1:27017/'
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(local_MongoDB_url,{
+        useNewUrlParser: true
+    });
+
+    return client.db('node_mongodb').collection('posts');
+
+}
+
 //get post
 router.get('/', async (req,res)=>{
     const posts = await loadPostsCollection();
@@ -27,13 +51,16 @@ router.delete('/:id', async (req, res)=>{
     res.status(200).send();
 });
 
-async function loadPostsCollection() {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://alif:zhorif@clusteralif.c1gdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-        useNewUrlParser: true
-    });
+//Update Post
+router.put('/:id', async(req,res)=>{
+    const posts = await loadPostsCollection();
+    var myQuery = {_id: new mongodb.ObjectID(req.params.id)};
+    var newValue = {$set:{text: req.body.text}};
+    await posts.updateOne(myQuery, newValue);
+    console.log('update success');
+    res.send(200).send();
+});
 
-    return client.db('vue_express').collection('posts');
 
-}
 
 module.exports = router;
